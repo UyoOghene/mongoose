07ed43c1f62ce0849d1d447b3ec5f4038746c2d1
Uyooghene <uyooghene01@gmail.com>
2024-10-27 19:15:54 +0100

    validation, default

--- index.js
+++ index.js
@@ -18,4 +18,30 @@ const movieSchema = new mongoose.Schema({
 
 const Movie = mongoose.model('Movie',movieSchema)
 
-const Harrypotter = new Movie({ title: 'Harrypotter', year: 2045, score: 8.4, rating: 'fourty' });
+// const Harrypotter = new Movie({ title: 'Harrypotter', year: 2045, score: 8.4, rating: 'fourty' });
+
+// Movie.insertMany([
+//   {title: 'Harrypotter', year: 2045, score: 8.4, rating: 'fourty'},
+//   {title: 'GOT', year: 1945, score: 8.4, rating: 'fourty'},
+//  { title: 'Princenme', year: 2025, score: 8.4, rating: 'fourty'},
+//  { title: 'Cinderella', year: 2345, score: 8.4, rating: 'fourty'},
+//  { title: 'swan princss', year: 1935, score: 8.4, rating: 'fourty'},
+// ])
+// .then(data =>{
+//   console.log('it worked')
+//   console.log(data)
+// })
+
+// in node
+Movie.find({title : 'Princenme'}).then(data => console.log(data))
+Movie.find({year : {$gte: 2000 }}).then (data => console.log(data))
+Movie.find({year : {$lt: 2000 }}).then (data => console.log(data))
+Movie.updateOne({title : 'Princenme'},{year : 1900}).then(res => console.log(res))
+Movie.updateMany({title: {$in: ['swan princss', 'GOT']}}, {score: 33}).then(res => console.log(res))
+Movie.findOneAndUpdate({title: 'GOT'}, {score: 400}, {new:true}).then(m =>console.log(m) )
+Movie.deleteOne({ title: 'swan princss' }).then(msg => console.log(msg));
+Movie.deleteMany({year : {$gt :2025}}).then(data => console.log(data))
+
+// in mongosh
+db.movies.find({title: {$in: ['Cinderella','GOT']}})
+db.movies.find({})
---
+++ product.js
@@ -0,0 +1,101 @@
+// const mongoose = require('mongoose')
+// mongoose.connect('mongodb://localhost:27017/shopApp')
+//     .then(() => {
+//         console.log('connection open');
+//     })
+//     .catch(err => {
+//         console.log('oh no error!');
+//         console.log(err);
+//     });
+//     const productschema = new mongoose.Schema({
+//         name: {
+//             type: String,
+
+//         },
+//         price: {
+//             type: Number
+//         }
+//     })
+
+//     const product = mongoose.model('Product', productschema);
+// const bike = new product({name: "mountain bike", price: 390})
+// bike.save()
+// .then(data => {
+//     console.log('it worked')
+//     console.log(data)
+// })
+// .catch(err => {
+//     console.log(err)
+// })
+
+// db.products.find({})
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/shopApp')
+    .then(() => {
+        console.log('connection open');
+    })
+    .catch(err => {
+        console.log('oh no error!');
+        console.log(err);
+    });
+
+const productSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+        maxlength: 20
+    } ,
+    price:{
+        type: Number,
+        required: false,
+        min:0     
+    },
+    onSale :{
+       type: Boolean,
+       default: true
+
+    },
+    categories: [String],
+    qty: {
+        online: {
+            type: Number,
+            default: 0
+        },
+        inStore: {
+            type: Number,
+            default: 0
+        }
+    }
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+// const bike = new Product({ name: "bike shoes", price: 10, categories:['cycling','safety', 123]});
+// bike.save()
+//     .then(data => {
+//         console.log('it worked');
+//         console.log(data);
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     });
+Product.findOneAndUpdate(
+    { name: "bike shoes" },
+    { price: 300},
+    { new: true, runValidators: true }    
+)
+.then(data => {
+    console.log('Update successful');
+    console.log(data);
+})
+.catch(err => {
+    console.log('Error:', err);
+});
+
+//     mongosh
+// use shopApp               # Switch to the database where data is saved
+// show collections           # Check that 'products' is listed
+// db.products.find({})       # View all documents in the products collection
+// Product.deleteOne({ name: "mountain bike" }).then(msg => console.log(msg));
